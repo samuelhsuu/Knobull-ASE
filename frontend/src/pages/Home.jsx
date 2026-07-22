5e08cb6242c27cdf55de2401f3296800b5548724
samuelhsuu <hsusamuel01@gmail.com>
2026-07-22 18:34:30 -0400

    Fixed search bar rendering bug

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,21 @@
+import {useEffect} from 'react';
 import Navbar from "../components/Navbar"
 import "./Home.css"
 function Home(){
+	useEffect(() => {
+		const existingScript = document.querySelector('script[src*="https://cse.google.com/cse.js?cx=53d5ee3be0c6f4aa7"]');
+
+		if (!existingScript){
+			const script = document.createElement('script');
+			script.src = "https://cse.google.com/cse.js?cx=53d5ee3be0c6f4aa7";
+			script.async = true;
+			document.body.appendChild(script);
+		} else {
+			if (window.google && window.google.search){
+				window.google.search.cse.element.go();
+			}
+		}
+	}, []);
 	return (
 		<>
 			<Navbar/>
@@ -13,9 +28,8 @@ function Home(){
 					<img src="../images/edx-logo-elm.svg"/>
 					<img src="../images/chatwithus.png"/>
 					<img src="../images/jobstars.png"/>
+				</div>
 			</div>
-			</div>
-			
 		</>
 	);
 }
