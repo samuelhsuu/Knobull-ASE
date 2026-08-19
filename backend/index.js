7bce69dadf3925a536960824ed97299ddaaa093c
samuelhsuu <hsusamuel01@gmail.com>
2026-08-18 22:44:27 -0400

    Fixed backend index.js

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ import { pipeline } from '@huggingface/transformers';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express()
 const PORT = process.env.PORT || 5000;
 
